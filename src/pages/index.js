3cf7e65a3184024c9102ff157fb044bb1b9876ac
Rhys <rbrooker105@gmail.com>
2018-04-25 16:15:30 +1000

    add main banner to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,49 @@
 import React from "react";
 import styled from "styled-components";
-
-//import Container from "../components/container";
-//<h1> tag with some styles
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
+import harbourImage from '../assets/harbour.jpg'
 
 // Create a Wrapper component that'll render a <section> tag with some styles
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
-// Use Title and Wrapper like any other React component – except they're styled!
-const IndexPage = () => (
-  <Wrapper>
-    <Title>
-      Hello World, this is my first styled component! pages/index.js/
-    </Title>
-  </Wrapper>
-);
+const Wrapper = styled.div`
+  position: relative;
+  height: 20em;
+  background: url(${harbourImage});
+  background-position: 50% 50%;
+  background-size: cover;
+  text-align: center`;
+
+const Heading = styled.h2`
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: auto;
+  top: 2.5em;
+  color: #fff;
+  font-family: arial, sans-serif;
+  font-size: 1.8em;
+  letter-spacing: 0.0675em;
+  font-weight: 100`;
+
+const Tagline = styled.h3`
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: auto;
+  top: 7em;
+  color: #fff;
+  font-family: arial, sans-serif;
+  font-size: 1.3em;
+  letter-spacing: 0.0725em;
+  font-weight: 100`;
+
+class IndexPage extends React.Component {
+  render () {
+    return (
+      <Wrapper>
+        <Heading>Front End Developer</Heading>
+        <Tagline>Improving the world wide web pixel by pixel...</Tagline>
+      </Wrapper>
+    )
+  }
+}
 
 export default IndexPage; 
