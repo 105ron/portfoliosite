a612956929bd4d170278528ca6b04f17192a6223
Rhys <rbrooker105@gmail.com>
2018-06-07 14:01:07 +0800

    troubleshoot profile photo bug

--- src/components/HelloCard.js
+++ src/components/HelloCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from "styled-components";
 import Device from '../assets/mediaqueries';
-import rhysImage from '../assets/rhys.jpg'
+import rhysImage from '../assets/rhys.jpg';
+
 const Card = styled.div`
-  height: 35px;
   margin-top: 1.8rem;
-  background-color: #e6e6e6;`
+  background-color: #e6e6e6;
+  height:1200px`
 
 const ProfilePhoto = styled.image`
   border: 4px solid black;
@@ -15,7 +16,7 @@ const ProfilePhoto = styled.image`
 function HelloCard () {
   return (
   <Card>
-    <ProfilePhoto src={rhysImage} alt="Profile Photo" height="500" width="500" />
+    <ProfilePhoto src={rhysImage} alt="Profile Photo" />
   </Card>
   )
 }
