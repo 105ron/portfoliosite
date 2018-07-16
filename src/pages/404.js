fa17cae0e44b79836bddcb0ea95c82855e198b23
Rhys <rbrooker105@gmail.com>
2018-07-16 13:56:31 +0800

    lint social-icons.js and add content to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from "styled-components";
+import sadFace from '../assets/sad.svg';
+import BannerImage from '../components/BannerImage';
+import Device from '../assets/mediaqueries';
 
-const NotFoundPage = () => (
-  <div>
-    <h1>Uh oh</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)
+const Wrapper = styled.div`
+  text-align: center;
+  max-width: var(--maxwidth);
+  margin: 1.8rem auto;
+  padding: 0 20px;
+`;
 
-export default NotFoundPage
+const SadFace = styled.img`
+  height: 500px;
+  width: 500px;
+  @media ${Device.tablet} {
+    height: 250px;
+    width: 250px;
+  }
+`;
+
+function NotFoundPage(props) {
+  const { bannerImage } = props;
+  return (
+    <div>
+      <BannerImage
+        heading="Error"
+        tagline="Page not found"
+        image={bannerImage}
+        alt="Sydney harbour banner image"
+      />
+      <Wrapper>
+        <SadFace src={sadFace} alt="Sad face" />
+        <p>
+          The page you requested doesn&#39;t exist. Did you click a link to get here?
+          Please click the email icon below and tell me how you got here. Thanks, Rhys.
+        </p>
+      </Wrapper>
+    </div>
+  );
+}
+
+NotFoundPage.propTypes = {
+  bannerImage: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired,
+};
+
+export default NotFoundPage;
+
+/* eslint import/no-extraneous-dependencies: "off" */
