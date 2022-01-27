f6c8f2bf9e9913650d6a2df11c58977e2aa4a083
Rhys <rbrooker105@gmail.com>
2022-01-27 20:59:59 +1100

    update app to nextjs

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from '../styles.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.hello}>
+      <p>Coming Soon!</p>
+    </div>
+  )
+}
--- src/assets/helpers/dateFormatter.js
+++
@@ -1,7 +0,0 @@
-function formatDate(publishedDate) {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  const published = new Date(publishedDate);
-  return published.toLocaleDateString('en-US', options);
-}
-
-export default formatDate;
